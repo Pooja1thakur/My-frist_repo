cb895f4287b4f980c2b0ff6609ef075a6601eb5e
Pooja1thakur <pooja2810rohit@gmail.com>
2024-11-29 12:51:09 +0530

    pattren printing

---
+++ dsa pattern printing/script.js
@@ -0,0 +1,92 @@
+//pattern parinting
+
+
+//function fun(n){
+ // let bag=""
+ // for(let i=1;i<=n;i++){
+ //   bag+="*"+" "
+ // }
+ // console.log(bag)
+//}
+//fun(6);
+
+
+
+//l shape pattern pringting
+//let n=5;
+//for(let i=1;i<=n-1;i++){
+//console.log("*")
+
+//}
+
+ // let bag=""
+ // for(let i=1;i<=n;i++){
+ //   bag+="*"+" "
+ // }
+  
+ // console.log(bag);
+  
+  
+  
+  
+//u shape printing pattern  
+// let n=6;
+// let bag=""
+// for(let i=1;i<=n;i++){
+// bag+="*"+" "
+
+// }
+// console.log(bag)
+
+// let bag2="*";
+// for(let i=1;i<=2*(n-2);i++){
+//   bag2+=" "
+//   //console.log(bag2)
+// }
+// bag2+=" *"
+// for(let i=1;i<=n-2;i++){
+// //   console.log(bag2)
+// // }
+// // let bag1=""
+// // for(let i=1;i<=n;i++){
+// // bag1+="*"+" "
+
+// // }
+// // console.log(bag1)
+
+
+
+
+
+
+// //number pattern 
+// let n=5;
+
+// // for(let i=1;i<=n;i++){
+// //   let bag=""
+// //   for(let j=1;j<=i;j++){
+// //       bag+=j
+// //   }
+// // console.log(bag)
+// // }
+
+// //    another method 1
+// let bag=""
+// for(let i=1;i<=n;i++){
+//   bag+=i
+//   console.log(bag)
+// }
+
+
+
+// let bag1=""
+// for(let i=1;i<=n-1;i++){
+//   bag1+=i
+//   console.log(bag1)
+// }
+
+
+
+// //print the binary square.
+// let n=2;
+  
